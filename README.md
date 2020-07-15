e2fdf9f7cc0a50c76b8a2ae79f9b683f4e333ebe
mjvinti <mjvinti@gmail.com>
2020-07-15 14:48:51 -0400

    init commit, reversetring test suite added

---
+++ README.md
@@ -0,0 +1,21 @@
+# Algo Cardio
+
+Coding / algorithm exercises
+
+# Enivornment Setup
+
+```sh
+$ npm install -g jest
+```
+If on mac, include `sudo`
+
+# Repo Test Setup
+```sh
+$ cd exercises
+$ jest
+```
+
+To run jest on single test suite
+```sh
+$ jest [test suite]
+```
---
+++ exercises/reversestring/index.js
@@ -0,0 +1,12 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('hello') === 'olleh'
+//   reverse('there') === 'ereht'
+//   reverse('apple') === 'leppa'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {};
+
+module.exports = reverse;
