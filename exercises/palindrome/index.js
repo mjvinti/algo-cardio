2e09ac2a2543dfe2f31ae182724b3f8837e41d7b
mjvinti <mjvinti@gmail.com>
2020-07-20 18:03:20 -0400

    added palindrome test cases

---
+++ exercises/palindrome/index.js
@@ -0,0 +1,14 @@
+// --- Directions
+// A palindrome is a string that forms the same word if it is
+// reversed. Given a string, return true if the string is a
+// palindrome; return false if it is not.
+// --- Examples:
+//   palindrome("racecar") === true
+//   palindrome("notapalindrome") === false
+//  --- SPECIAL INSTRUCTIONS
+// INCLUDE spaces and punctuation in determining if a string
+// is a palindrome.
+
+function palindrome(str) {};
+
+module.exports = palindrome;
